6870ac62cd3db00b12389aa8b01266d4a367ad34
Brock Lebeau <brocklebeau338@gmail.com>
2021-11-23 18:57:24 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+
+          src='src/Instagram_logo.png'
+          alt=""
+        ></img>
+      </div>
+
+      {/* Header */}
+
+
+      {/* Posts */}
+      {/* Posts */}
+
+    </div>
+  );
+}
+
+export default App;
